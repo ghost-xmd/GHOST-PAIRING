ab8337492ef4703f3bc70bd5dd0b6b86f4fcb4ff
ghost-xmd <ghostviewer99@gmail.com>
2025-11-10 10:09:35 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,17 +81,17 @@ router.get('/', async (req, res) => {
 
                         // Send video thumbnail with caption
                         await KnightBot.sendMessage(userJid, {
-                            image: { url: 'https://img.youtube.com/vi/-oz_u1iMgf8/maxresdefault.jpg' },
-                            caption: `🎬 *KnightBot MD V2.0 Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/-oz_u1iMgf8`
+                            image: { url: 'https://files.catbox.moe/cv6t5y.jpeg' },
+                            caption: `🎬 *GHOST-XMD Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: http://www.youtube.com/@HACKLINKTECH-h4h`
                         });
                         console.log("🎬 Video guide sent successfully");
 
                         // Send warning message
                         await KnightBot.sendMessage(userJid, {
                             text: `⚠️Do not share this file with anybody⚠️\n 
-┌┤✑  Thanks for using Knight Bot
+┌┤✑  Thanks for using GHOST-XMD
 │└────────────┈ ⳹        
-│©2024 Mr Unique Hacker 
+│©2025 GHOST-VIEWER
 └─────────────────┈ ⳹\n\n`
                         });
                         console.log("⚠️ Warning message sent successfully");
